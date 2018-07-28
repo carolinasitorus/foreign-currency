735bfd62226f188ee2303b578bea75a4b9cf2481
Carolina Sitorus <if413061@gmail.com>
2018-07-28 14:32:16 +0700

    Add foreing currency table migration

---
+++ foreigncurrencyapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class ForeignCurrency(models.Model):
+    currency_name = models.CharField(max_length=25, blank=True, null=True)
+    description = models.CharField(max_length=225, blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'foreign_currencies'
+
+    def __str__(self):
+        return self.currency
